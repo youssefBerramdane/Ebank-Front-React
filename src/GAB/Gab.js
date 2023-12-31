5d3e23b1de47450886522816506a71fe8970fd2b
youssefberramdane <youssef.figuig07@gmail.com>
2023-12-31 14:55:00 +0100

    devGab

--- src/GAB/Gab.js
+++ src/GAB/Gab.js
@@ -3,11 +3,33 @@ import React, { useState } from 'react'
 import style from './Gab.module.css';
 export default function Gab() {
     const [reference,setReference]=useState("");
+    const [current,setCurrent]=useState("reference");
+    const [pin,setPin]=useState("");
     const changeRef=(num)=>{
-        setReference(reference+""+num)
+        if(current==="reference"){
+            setReference(reference+""+num)
+        }else if(current=="PIN"){
+            setPin(pin+""+num)
+        }
+        
     }
     const resetRef=()=>{
+        if(current==="reference"){
         setReference("")
+        }else{
+            setPin("")
+        }
+    }
+    const checkRef=()=>{
+        if(current==="reference"){
+            setCurrent("PIN")
+        }
+        
+    }
+    const cancelOperation=()=>{
+        setCurrent("reference");
+        setReference("");
+        setPin("");
     }
   return (
     <div className={style.page}>
@@ -16,7 +38,9 @@ export default function Gab() {
                     <div className="border bg-white" {...style.gabscreen}>
                 
                         <div id="screen" className="p-3">
-                            {reference.length>0?reference:"Bonjour inserez votre référence"}
+                            {current==="reference" && (reference.length>0?reference:"Bonjour inserez votre référence")}
+                            {current==="PIN" && (pin.length>0?pin:"Bonjour inserez votre Code Pin")}
+                        
                         </div>
                     </div>
                     <div className="mt-3" {...style.gabbuttons}>
@@ -24,7 +48,6 @@ export default function Gab() {
                             
                         <div className="col">
                             <div className={style.numberpad}>
-                            
                                 <button onClick={()=>changeRef(1)} className="btn btn-secondary">1</button>
                                 <button onClick={()=>changeRef(2)} className="btn btn-secondary">2</button>
                                 <button onClick={()=>changeRef(3)} className="btn btn-secondary">3</button>
@@ -36,7 +59,8 @@ export default function Gab() {
                                 <button onClick={()=>changeRef(9)} className="btn btn-secondary">9</button>
                                 <button onClick={()=>changeRef(0)} className="btn btn-secondary">0</button>
                                 <button onClick={()=>resetRef()} className="btn btn-danger">Cancel</button>
-                                <button className="btn btn-success">Enter</button>
+                                <button onClick={checkRef} className="btn btn-success">{current==="reference"?"suivant":"valider"}</button>
+                                {current==="PIN" && <button onClick={cancelOperation} className="btn btn-danger">Annuler l'opération</button>}
                             </div>
                         </div>
                     </div>
